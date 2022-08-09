87008436b5bc0f1cf8b6df7685113bf884ffd710
Christian Liebhardt <christian.liebhardt@arcor.de>
2022-08-09 16:53:43 +0200

    yarn run export

---
+++ _next/static/UrcY0ekXAsGM8dJJI7An2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/10-1f045445020b55b4.js","static/chunks/pages/index-008e26a8b665197c.js"],"/_error":["static/chunks/pages/_error-b5352164729facd9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
